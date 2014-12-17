64b3f36e4e69d5eb2591ec9c94226cb0fb47d46f
mntj <mntj11@gmail.com>
2014-12-17 00:37:46 -0500

    Add handling for multiple line inputs. Remove activesupport gem

--- main.rb
+++ main.rb
@@ -9,11 +9,19 @@ end
 post '/' do
   code = params[:code]
   @@p.eval(code)
+
+  # Exception
   if @@p.instance_variable_get("@last_result_is_exception")
-    @@p.instance_variable_get("@last_exception").inspect.to_s
-  elsif @@p.instance_variable_get("@eval_string").empty?
-    @@p.output_array[-1].to_json
+    @@p.instance_variable_get("@last_exception").inspect
+  # Ongoing input
+  elsif !@@p.instance_variable_get("@eval_string").empty?
+    "* " << code
+  # End of input
   else
-   ("*" + @@p.instance_variable_get("@eval_string")).to_json
+    if @@p.output_array[-1].class == Symbol
+      ":" + @@p.output_array[-1].to_s
+    else
+      @@p.output_array[-1].to_s
+    end
   end
 end
