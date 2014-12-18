bf0c349843f95c0220e28220af12df0ab97021be
mntj <mntj11@gmail.com>
2014-12-18 02:55:53 -0500

    Add IO work

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'stringio'
 require 'bundler'
 Bundler.require
 
@@ -8,20 +9,36 @@ end
 
 post '/' do
   code = params[:code]
-  @@p.eval(code)
 
-  # Exception
-  if @@p.instance_variable_get("@last_result_is_exception")
-    @@p.instance_variable_get("@last_exception").inspect
-  # Ongoing input
-  elsif !@@p.instance_variable_get("@eval_string").empty?
-    "* " << code
-  # End of input
-  else
-    if @@p.output_array[-1].class == Symbol
-      ":" + @@p.output_array[-1].to_s
-    else
-      @@p.output_array[-1].to_s
-    end
-  end
+  $stdout = StringIO.new
+
+  @@p.eval(code)
+  output = $stdout.string
+
+  $stdout = STDOUT
+
+  output_arr = []
+  output.each_line('\n') { |line| output_arr << line }
+
+  output_arr[1]
+
+  binding.pry
+  # binding.pry
+  # # Exception
+  # if @@p.instance_variable_get("@last_result_is_exception")
+  #   binding.pry
+  #   @@p.instance_variable_get("@last_exception").inspect
+  # # Ongoing input
+  # elsif !@@p.instance_variable_get("@eval_string").empty?
+  #   binding.pry
+  #   "* " << code
+  # # End of input
+  # else
+  #   binding.pry
+  #   if @@p.output_array[-1].class == Symbol
+  #     ":" + @@p.output_array[-1].to_s
+  #   else
+  #     @@p.output_array[-1].to_s
+  #   end
+  # end
 end
