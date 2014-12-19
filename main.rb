12eb2950fd53ce99da51f9fc8aba529b2f393eb0
mntj <mntj11@gmail.com>
2014-12-19 14:33:34 -0500

    Add activesupport

--- main.rb
+++ main.rb
@@ -7,23 +7,22 @@ get '/' do
 end
 
 post '/' do
-  content_type :json
-  code = params[:code]
+  # content_type :json
+  # code = params[:code]
 
-  def capture_stdout
-    begin
-      old_stdout = $stdout
-      $stdout = StringIO.new('','w')
-      yield
-      $stdout.string
-    ensure
-      $stdout.close
-      $stdout = old_stdout
-    end
-  end
+  # def capture_stdout
+  #   begin
+  #     old_stdout = $stdout
+  #     $stdout = StringIO.new('','w')
+  #     yield
+  #     $stdout.string
+  #   ensure
+  #     $stdout = old_stdout
+  #   end
+  # end
 
-  output = capture_stdout { @@p.eval(code) }
+  # output = capture_stdout { @@p.eval(code) }
 
-  puts output
+  # puts output
 end
 
