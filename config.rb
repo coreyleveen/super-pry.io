6f19a8c68adeee2e3c060ec006805a3f63ec9098
Corey Leveen <mntj11@gmail.com>
2015-03-03 23:28:11 -0500

    Fixing Kernel issues

--- config.rb
+++ config.rb
@@ -4,18 +4,18 @@ require 'active_support/core_ext'
 class PermissionsError < Exception
 end
 
-module Kernel
-  def `(*)
-    raise PermissionsError, "I'm sorry Dave, I'm afraid I can't do that."
-  end
+# module Kernel
+#   def `(*)
+#     raise PermissionsError, "I'm sorry Dave, I'm afraid I can't do that."
+#   end
 
-  def system(*)
-    raise PermissionsError, "This mission is too important for me to allow you to jeopardize it."
-  end
+#   def system(*)
+#     raise PermissionsError, "This mission is too important for me to allow you to jeopardize it."
+#   end
 
-  def exec(*)
-    raise PermissionsError, "Access Denied."
-  end
-end
+#   def exec(*)
+#     raise PermissionsError, "Access Denied."
+#   end
+# end
 
 Kernel.freeze # Keep users from redefining Kernel methods.
--- main.rb
+++ main.rb
@@ -14,23 +14,24 @@ post '/' do
 end
 
 def process_command(input:)
-  old_output_size = $p.output_array.size
+  # old_output_size = $p.output_array.size
   stdout_stream = capture(:stdout) { $p.eval(input) }
   stderr_stream = capture(:stderr) { $q.eval(input) }
-  new_output_size = $p.output_array.size
+  # new_output_size = $p.output_array.size
   handler(
-    old_size: old_output_size,
-    new_size: new_output_size,
+    # old_size: old_output_size,
+    # new_size: new_output_size,
     stdout: stdout_stream,
     stderr: stderr_stream
   )
 end
 
 def handler(options)
+  binding.pry
   if !$p.eval_string.empty?
     "*"
-  elsif options[:new_size] != options[:old_size]
-    $p.output_array[-1].inspect
+  # elsif options[:new_size] != options[:old_size]
+    # $p.output_array[-1].inspect
   elsif options[:stdout_stream]
     options[:stdout_stream]
   else
