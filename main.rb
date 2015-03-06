818d72f4fa3aabcf34e0baf27bb2dd3804d2181e
Corey Leveen <mntj11@gmail.com>
2015-03-05 23:48:20 -0500

    Add to decolor

--- main.rb
+++ main.rb
@@ -19,18 +19,18 @@ def process_command(input:)
   handle(stdout_stream, stderr_stream)
 end
 
-def decolor(str)
-  if str =~ /\[1;36m/
-    str.gsub("[1;36m", "")
-  end
-end
-
 def handle(stdout, stderr)
   if !$p.eval_string.empty?
     "*"
   elsif stdout
     decolor(stdout)
   else
-    stderr
+    decolor(stderr)
   end
+end
+
+def decolor(str)
+  str.gsub!("=> \e[1;36m", "") if str =~ /=> \e\[1;36m/
+  str.gsub!("\e[0m","") if str =~ /\e\[0m/
+  str.gsub!("=> \e[33m", "") if str =~ /=> \e\[33m/
 end
