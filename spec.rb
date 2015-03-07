e09cfaba23d0de15f3a0ed5c729dbb75e5e29bda
Corey Leveen <mntj11@gmail.com>
2015-03-06 23:05:04 -0500

    More specs

--- spec.rb
+++ spec.rb
@@ -7,5 +7,15 @@ class MyTest < Minitest::Test
       cyan_str = "=> \e[1;36m"
       decolor(cyan_str).must_equal ""
     end
+
+    it 'removes yellow' do
+      yellow_str = "=> \e[33m"
+      decolor(yellow_str).must_equal ""
+    end
+
+    it 'removes trailing characters' do
+      str = "\e[0m"
+      decolor(str).must_equal ""
+    end
   end
 end
