baebf0fd578ba5e6de5812b5af7158ad5111967d
Corey Leveen <mntj11@gmail.com>
2015-03-20 20:03:49 -0400

    Remove Kernel method redefinitions

--- config.rb
+++ config.rb
@@ -1,21 +1,2 @@
 require 'active_support'
 require 'active_support/core_ext'
-
-class PermissionsError < Exception
-end
-
-module Kernel
-  # def `(*)
-  #   raise PermissionsError, "I'm sorry Dave, I'm afraid I can't do that."
-  # end
-
-  def system(*)
-    raise PermissionsError, "This mission is too important for me to allow you to jeopardize it."
-  end
-
-  def exec(*)
-    raise PermissionsError, "Access Denied."
-  end
-end
-
-Kernel.freeze # Keep users from redefining Kernel methods.
